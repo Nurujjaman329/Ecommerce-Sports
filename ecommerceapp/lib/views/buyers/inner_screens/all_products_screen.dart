779b1e1b956bc21e6a559ae1ed759cbfa8a112ae
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-17 00:50:39 +0600

    Category Final

--- ecommerceapp/lib/views/buyers/inner_screens/all_products_screen.dart
+++ ecommerceapp/lib/views/buyers/inner_screens/all_products_screen.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../productDetail/product_detail_screen.dart';
 
 class AllProductScreen extends StatelessWidget {
   final dynamic categoryData;
@@ -9,6 +10,10 @@ class AllProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance
+        .collection('products')
+        .where('category', isEqualTo: categoryData['categoryName'])
+        .snapshots();
     return Scaffold(
       appBar: AppBar(
         elevation: 1,
@@ -20,6 +25,85 @@ class AllProductScreen extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _productsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Something went wrong'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Colors.yellow.shade900,
+              ),
+            );
+          }
+
+          return GridView.builder(
+              itemCount: snapshot.data!.size,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                childAspectRatio: 200 / 300,
+              ),
+              itemBuilder: (context, index) {
+                final productData = snapshot.data!.docs[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return ProductDetailScreen(
+                        productData: productData,
+                      );
+                    }));
+                  },
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 170,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(productData['imageUrl'][0]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            productData['productName'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '\$' +
+                                ' ' +
+                                productData['productPrice'].toStringAsFixed(2),
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4,
+                              color: Colors.yellow.shade900,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              });
+        },
       ),
     );
   }
