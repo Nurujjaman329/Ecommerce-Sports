1ce15c040818ee0d231740a0486fe0eb309305b7
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-21 00:00:54 +0600

    TabBarView

--- ecommerceapp/lib/vendor/views/auth/screens/edit_product_screen.dart
+++ ecommerceapp/lib/vendor/views/auth/screens/edit_product_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerceapp/vendor/views/auth/screens/edit_products_tabs/published_tab.dart';
+import 'package:ecommerceapp/vendor/views/auth/screens/edit_products_tabs/unpublished_tab.dart';
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatelessWidget {
@@ -6,31 +8,38 @@ class EditProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            centerTitle: true,
-            backgroundColor: Colors.yellow.shade900,
-            title: Text(
-              'Manage Products',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 7,
-              ),
-            ),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  child: Text('Published'),
-                ),
-                Tab(
-                  child: Text('Unpublished'),
-                ),
-              ],
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.yellow.shade900,
+          title: Text(
+            'Manage Products',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 7,
             ),
           ),
-        ));
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Text('Published'),
+              ),
+              Tab(
+                child: Text('Unpublished'),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            PublishedTab(),
+            UnpublishedTab(),
+          ],
+        ),
+      ),
+    );
   }
 }
