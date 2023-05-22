df10ef81852bb536144588d9cbde815c97cb2116
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-22 21:58:27 +0600

    Prepopulating Forms

--- ecommerceapp/lib/vendor/views/auth/screens/vendorProductDetail/vendor_product_detail_screens.dart
+++ ecommerceapp/lib/vendor/views/auth/screens/vendorProductDetail/vendor_product_detail_screens.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class VendorProductDetailScreen extends StatelessWidget {
+class VendorProductDetailScreen extends StatefulWidget {
   final dynamic productData;
 
   const VendorProductDetailScreen({super.key, required this.productData});
+
+  @override
+  State<VendorProductDetailScreen> createState() =>
+      _VendorProductDetailScreenState();
+}
+
+class _VendorProductDetailScreenState extends State<VendorProductDetailScreen> {
+  final TextEditingController _productNameController = TextEditingController();
+
+  @override
+  void initState() {
+    setState(() {
+      _productNameController.text = widget.productData['productName'];
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +29,20 @@ class VendorProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.yellow.shade900,
         elevation: 0,
-        title: Text(productData['productName']),
+        title: Text(widget.productData['productName']),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _productNameController,
+              decoration: InputDecoration(
+                labelText: 'Product Name',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
