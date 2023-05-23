b42857cfdb2dcbd045d1ce71ad75c9be38c04966
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-23 16:52:00 +0600

    Display Vendor Details

--- ecommerceapp/lib/vendor/views/auth/screens/earning_screen.dart
+++ ecommerceapp/lib/vendor/views/auth/screens/earning_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class EarningScreen extends StatelessWidget {
@@ -5,8 +7,57 @@ class EarningScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Earning Screen"),
+    CollectionReference users = FirebaseFirestore.instance.collection('vendor');
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(FirebaseAuth.instance.currentUser!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              title: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(data['storeImage']),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Welcome Back ' + data['bussinessName'],
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            letterSpacing: 3),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+
+        return Center(
+          child: CircularProgressIndicator(
+            color: Colors.yellow.shade900,
+          ),
+        );
+      },
     );
   }
 }
