5a3a6a621c8b50958bd9fdff634eedd657192ae7
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-24 17:12:02 +0600

    Withdraw Validation

--- ecommerceapp/lib/vendor/views/auth/screens/vendor_inner_screens/withdrawal_screens.dart
+++ ecommerceapp/lib/vendor/views/auth/screens/vendor_inner_screens/withdrawal_screens.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class WithdrawalScreens extends StatelessWidget {
+class WithdrawalScreens extends StatefulWidget {
+  @override
+  State<WithdrawalScreens> createState() => _WithdrawalScreensState();
+}
+
+class _WithdrawalScreensState extends State<WithdrawalScreens> {
   late String amount;
+
   late String name;
+
   late String mobile;
+
   late String bankName;
+
   late String bankAccountName;
+
   late String bankAccountNumber;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -29,87 +39,140 @@ class WithdrawalScreens extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              TextFormField(
-                onChanged: (value) {
-                  amount = value;
-                },
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Amount',
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  name = value;
-                },
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  mobile = value;
-                },
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Mobile',
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  bankName = value;
-                },
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'Bank Details',
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  bankAccountName = value;
-                },
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'Bank Account Name',
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  bankAccountNumber = value;
-                },
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Bank Account Number',
-                ),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Get Cash',
-                  style: TextStyle(
-                    fontSize: 18,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please Field must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    amount = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Amount',
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please Name must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Mobile Number must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    mobile = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Mobile',
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Bank Name must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    bankName = value;
+                  },
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Bank Details',
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please Field must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    bankAccountName = value;
+                  },
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Bank Account Name',
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please Field must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    bankAccountNumber = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Bank Account Number',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ////STORE DATA IN CLOUD FIRESTORE
+                      ///
+                      print('True');
+                    } else {
+                      print('False');
+                    }
+                  },
+                  child: Text(
+                    'Get Cash',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
