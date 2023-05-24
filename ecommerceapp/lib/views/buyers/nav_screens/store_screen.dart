b403e258406c11c87ddcdca73a92dc4852c87f72
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-24 23:44:19 +0600

    Vendor Stores

--- ecommerceapp/lib/views/buyers/nav_screens/store_screen.dart
+++ ecommerceapp/lib/views/buyers/nav_screens/store_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class StoreScreen extends StatelessWidget {
@@ -5,10 +6,37 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StoreScreen'),
-      ),
+    final Stream<QuerySnapshot> _vendorsStream =
+        FirebaseFirestore.instance.collection('vendor').snapshots();
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _vendorsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return Container(
+          height: 500,
+          child: ListView.builder(
+              itemCount: snapshot.data!.size,
+              itemBuilder: (context, index) {
+                final storeData = snapshot.data!.docs[index];
+
+                return ListTile(
+                  title: Text(storeData['bussinessName']),
+                  subtitle: Text(storeData['countryValue']),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(storeData['storeImage']),
+                  ),
+                );
+              }),
+        );
+      },
     );
   }
 }
