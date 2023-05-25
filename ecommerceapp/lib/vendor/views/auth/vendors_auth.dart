177b19daaf7821e1fb254369a613fc80c99a6694
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-05-25 11:34:05 +0600

    FirebaseUi Auth Package

--- ecommerceapp/lib/vendor/views/auth/vendors_auth.dart
+++ ecommerceapp/lib/vendor/views/auth/vendors_auth.dart
@@ -1,9 +1,7 @@
 import 'package:ecommerceapp/vendor/views/auth/screens/landing_screen.dart';
-import 'package:ecommerceapp/vendor/views/auth/screens/main_vendor_screens.dart';
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:flutter/material.dart';
-
-import 'package:flutterfire_ui/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class VendorAuthScreen extends StatelessWidget {
   const VendorAuthScreen({super.key});
@@ -12,20 +10,15 @@ class VendorAuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
-      // If the user is already signed-in, use it as initial data
       initialData: FirebaseAuth.instance.currentUser,
       builder: (context, snapshot) {
-        // User is not signed in
-        if (!snapshot.hasData) {
-          return SignInScreen(providerConfigs: [
-            EmailProviderConfiguration(),
-            //GoogleProviderConfiguration(
-            //    clientId: '1:573827726190:android:7ed7cfbd8c5ffbdafa5f7f'),
-            //PhoneProviderConfiguration(),
-          ]);
+        if (snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+          );
         }
-
-        // Render your application if authenticated
         return LandingScreen();
       },
     );
